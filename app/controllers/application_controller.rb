3c167ea6bd2238fd371e6a554f6d541490a7ddeb
Ryan Johns <ryan.r.johns@gmail.com>
2017-10-26 06:48:00 +1100

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def hello
+    render html: "Hello my name is Matty Barker and i smell and chew my fingers"
+  end
 end
